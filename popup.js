6acf346ac5269cc95a34fc64d9a5c4c813186f4d
MoreMoople <46589328+MoreMoople@users.noreply.github.com>
2023-11-09 15:09:55 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
 // content.js
-const navbar = document.createElement('div');
-navbar.innerHTML = '<h2>This is a custom navbar!</h2>';
-navbar.style.background = '#333';
-navbar.style.color = '#fff';
-navbar.style.padding = '10px';
-navbar.style.position = 'fixed';
-navbar.style.top = '0';
-navbar.style.left = '0';
-navbar.style.width = '100%';
-document.body.prepend(navbar);
+if (window.location.hostname.includes('craigslist.org')) {
+  const navbar = document.createElement('div');
+  navbar.innerHTML = '<h2>Craigslist Navbar Extension</h2>';
+  navbar.style.background = '#333';
+  navbar.style.color = '#fff';
+  navbar.style.padding = '10px';
+  navbar.style.position = 'fixed';
+  navbar.style.top = '0';
+  navbar.style.left = '0';
+  navbar.style.width = '100%';
+  document.body.prepend(navbar);
+}
+
 
