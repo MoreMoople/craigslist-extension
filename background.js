2f691c5641cac5e28d63478591ae99b6504b901a
kelseyann77 <79773318+kelseyann77@users.noreply.github.com>
2023-11-13 17:26:09 -0700

    Create background.js

---
+++ background.js
@@ -0,0 +1,13 @@
+var host = "http://pebbleparadise.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://lascruces.craigslist.org/*",
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
